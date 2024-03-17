46d5b2e38cd9a068d94095a82b2b74df154a9d3d
jincabia <140627796+jincabia@users.noreply.github.com>
2024-03-16 22:53:48 -0600

    Working fetch

--- app/page.js
+++ app/page.js
@@ -1,113 +1,109 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect } from 'react';
+import  {loadExercises} from './src/components/fetchExcercises';
+import { Excercise } from './src/excercise/exercise';
 
-export default function Home() {
+export default function App() {
+  const [exercises, setExercises] = useState([]);
+  const [muscle, setMuscles] = useState("Abdominals")
+  const [muscleName, setmuscleName] = useState ("");
+  // const apiKey = 'e29d3286f0msh7d1b166b41ec7b9p1a5505jsn8a40d290bfa8';
+
+  const changeMuscle = event =>
+  {
+    setMuscles(event.target.value);
+  }
+
+
+  const handleMuscleName = (muscle) =>
+  {
+    //capitalizing the first character 
+    const newName = (muscle.charAt(0).toUpperCase() + muscle.slice(1)).replace('_', ' ');
+    setmuscleName(newName);
+  }
+
+  
+
+  const handleSubmit = event =>
+  {
+    event.preventDefault();
+
+    // console.log(loadExercises);
+    loadExercises(muscle,setExercises,setmuscleName);
+    handleMuscleName(muscle);
+    
+    // setmuscleName(muscle);
+    
+
+  }
+ 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <h1>Fetch excercises</h1>
+      <h3>Search for Excercises by muscle</h3>
+
+      <form onSubmit={handleSubmit}>
+      <select id="dropdown" className='text-black font-family: Arial m-4 p-4' onChange={changeMuscle}
+        value={muscle}>
+        <option className='font-family: Arial' disabled>Select a muscle group</option>
+        <option value="abdominals" className='font-family: Arial'>Abdominals</option>
+        <option value="abductors" className='font-family: Arial'>Abductors </option>
+        <option value="adductors" className='font-family: Arial'>Adductors</option>
+        <option value="biceps" className='font-family: Arial'>Biceps</option>
+        <option value="calves" className='font-family: Arial'>Calves</option>
+        <option value="chest" className='font-family: Arial'>Chest</option>
+        <option value="forearms" className='font-family: Arial'>Forearms</option>
+        <option value="glutes" className='font-family: Arial'>Glutes</option>
+        <option value="hamstrings" className='font-family: Arial'>Hamstrings</option>
+        <option value="lats" className='font-family: Arial'>Lats</option>
+        <option value="lower_back" className='font-family: Arial'>Lower back</option>
+        <option value="middle_back" className='font-family: Arial'>Middle back</option>
+        <option value="neck" className='font-family: Arial'>Neck</option>
+        <option value="quadriceps" className='font-family: Arial'>Quadriceps</option>
+        <option value="traps" className='font-family: Arial'>Traps</option>
+        <option value="triceps" className='font-family: Arial'>Triceps</option>
+
+        
+      </select>
+        <button className='ml-4 py-3 px-20 bg-blue-500 text-white rounded hover:bg-blue-700'>
+                                Search
+                            </button>
+
+                
+      </form>
+      
+
+  
+
+      {/* Display Exercises */}
+      <div>
+      {muscleName ? (
+        <div>
+          <h2>Exercises for {muscleName}</h2>
         </div>
+      ) : (
+        <div>
+          <h2>Search for an exercise </h2>
+        </div>
+      )}
+
+
+  { exercises == null ||exercises.length == 0 ? (
+        <div>
+        <h2>No exercises found </h2>
       </div>
+        
+      ) : (
+        <div>
+          {exercises.map((exercise, index) => (
+          <Excercise name={exercise.name} id = {index} />
+        ))}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          
+        </div>
+     
+      )}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
---
+++ app/src/excercise/exercise.js
@@ -0,0 +1,12 @@
+export function Excercise({name, id}) {
+
+
+    return (        
+       <div className=" m-4 p-4 text-white bg-slate-900 rounded w-1/4 h-1/3 ">
+        <h2 className="font-bold text-xl">{name}</h2> 
+       </div>
+        
+      
+    );
+  }
+
